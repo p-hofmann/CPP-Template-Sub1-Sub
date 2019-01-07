4f6b5bd0a95a55a65937f9c39c4b7cdcaa150883
hofmann <p.hofmann.email@gmail.com>
2019-01-07 22:49:53 +0100

    Initial commit of source files.

---
+++ src/cpptemplatesubmodule1submodule_DARWIN.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Peter Hofmann on 07.01.19.
+//
+
+#include <iostream>
+#include "cpptemplatesubmodule1submodule.h"
+
+using namespace SubModule1;
+
+/**
+ * This method generates a string representing the class
+ * The OS specificity here is silly, but imagine instead a method like getCWD()
+ * @return - String representing the class
+ */
+std::string CPPTemplateSubmodule1Submodule::toString()
+{
+    _os = APPLE;
+    return "[" + getClassName() + "] OS: " + _osToString() + ", ARCH: " + std::to_string(_build_arch);
+}
